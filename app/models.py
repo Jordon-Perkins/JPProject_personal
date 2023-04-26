cf91e46bd4fa65123086af71b15f0a5ba4d07596
Jordon Perkins <jordon.rubel@gmail.com>
2023-04-26 14:00:51 -0500

    migrations fixed

--- app/models.py
+++ app/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=5000)
