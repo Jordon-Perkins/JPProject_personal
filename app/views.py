c586b9b3d226b508144446a7be878d3c37ae48ba
Jordon Perkins <jordon.rubel@gmail.com>
2023-04-26 15:35:06 -0500

    template kind of

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
 from .models import Project
-
 from django.http import HttpResponse
 
 
 def index(request):
     project_list = Project.objects.all()
     output = ", ".join([q.title for q in project_list])
-    return HttpResponse(output)
+    template = loader.get_template("app/index.html")
+    context = {
+        "project_list": project_list,
+    }
+    return HttpResponse(template.render(context, request))
