7d89859cfd890f544cf1c53409b6cc9f1ced2b5e
Jordon Perkins <jordon.rubel@gmail.com>
2023-04-26 14:59:09 -0500

    view/url

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Project
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    project_list = Project.objects.all()
+    output = ", ".join([q.title for q in project_list])
+    return HttpResponse(output)
